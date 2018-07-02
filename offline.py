d8470295cb53fe51a9c13a2c40332f7f602d8baf
ghostboyzone <ghostboyzone@gmail.com>
2018-07-02 18:29:52 +0800

    add exception catch

--- offline.py
+++ offline.py
@@ -6,9 +6,16 @@ from feature_extractor import FeatureExtractor
 
 fe = FeatureExtractor()
 
+idx = 0
+
 for img_path in sorted(glob.glob('static/img/*.jpg')):
-    print(img_path)
-    img = Image.open(img_path)  # PIL image
-    feature = fe.extract(img)
-    feature_path = 'static/feature/' + os.path.splitext(os.path.basename(img_path))[0] + '.pkl'
-    pickle.dump(feature, open(feature_path, 'wb'))
+	try:
+		idx = idx+1
+		print(img_path, idx)
+		img = Image.open(img_path)  # PIL image
+		feature = fe.extract(img)
+		feature_path = 'static/feature/' + os.path.splitext(os.path.basename(img_path))[0] + '.pkl'
+		pickle.dump(feature, open(feature_path, 'wb'))
+		pass
+	except Exception as e:
+		print(e)
